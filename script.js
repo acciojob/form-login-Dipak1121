61241cea1407585dc066c7aa336a544cf3c42124
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 12:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
-    //Write your code here
 
+function getFormvalue() {
+   event.preventDefault();
+	let form = event.target;
+	let firstName = form.fname.value;
+	let secondName = form.lname.value
+	alert(`${firstName} ${secondName}`)
+	
 }
